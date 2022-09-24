9db2357af86769dc5de68f67f933ff7ec668cff7
TyNoller4317 <tjjnoller@gmail.com>
2022-09-24 17:16:17 -0600

    started the footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navbar } from "./components/Navbar";
 import { HeroSection } from "./components/HeroSection";
 import { MakesUsSpecial } from "./components/MakesUsSpecial";
 import { InfoSection } from "./components/InfoSection";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HeroSection />
       <MakesUsSpecial />
       <InfoSection />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="top-footer">
+        <div className="footer-logo">
+          <h1>Axios.io</h1>
+          <p>
+            But i must explain to you how all this mistaken idea of demonizing
+            pleasure and praising born.
+          </p>
+        </div>
+        <div className="footer-links"></div>
+        <div className="footer-links"></div>
+        <div className="footer-links"></div>
+      </div>
+      <div className="bottom-footer"></div>
+    </footer>
+  );
+};
